c667e168757f198f8c30ad81777b86708704f404
Maxy-byte <maximilianreinhardt9@gmail.com>
2025-04-17 11:54:10 +0200

    Pi Erweiterung

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Start /  Code für Windows
+
 import cv2
 import numpy as np
 import time
@@ -11,8 +13,8 @@ model.conf = 0.5
 model.classes = [0]
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+cap.set(3, 1280)
+cap.set(4, 720)
 
 prev_time = time.time()
 
@@ -35,6 +37,7 @@ while True:
             print(f"[Serial Error] {ex}")
         
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        cv2.circle(frame, (center_x, center_y), 5,(255, 0, 0), -1)
         
     cv2.imshow("YOLO11n - Windows PyThorch Version", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -43,3 +46,52 @@ while True:
 cap.release()
 arduino.close()
 cv2.destroyWindow()
+
+#Ende
+
+#Start / Code für Pi
+
+# import time
+# import cv2
+# import serial
+# import torch
+# from picamera2 import Picamera2
+# from ultralytics import YOLO
+
+# arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+
+# picam2 = Picamera2()
+# picam2 = configure(picam2.create_preview_configuration(main={"format": "RGB888", "size": (1280, 720)}))
+# picam2.start()
+
+# model = YOLO("yolo11n.pt")
+
+# prev_time = time.time()
+
+# while True:
+#     frame = picam2.capture_array()
+
+#     results = model(frame, imgsz=640, conf=0.5)[0]
+
+#     for box in results:
+#         x1, y1, x2, y2 = map(int, box.boxes.xyxy[0].tolist())
+#         center_x = int((x1 + x2) / 2)
+#         center_y = int((y1 + y2) / 2)
+
+#         try:
+#             arduino.write(f"{center_x}, {center_y}\n".encode())
+#         except Exception as ex:
+#             print(f"[Serial Error] {ex}")
+        
+#         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+#         cv2.circle(frame, (center_x, center_y), 5,(255, 0, 0), -1)
+
+#         cv2.imshow("Raspberry YOLO", frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+
+# picam2.stop()
+# arduino.close()
+# cv2.destroyWindow()
+
+#Ende
